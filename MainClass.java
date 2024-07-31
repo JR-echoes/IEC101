fda9bcf88b1b502a192e60da08a177525c6300e5
JR-echoes <janardankonewid@gmail.com>
2024-07-31 19:12:41 +0545

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,10 @@
+package com.janardanrimal.jpt;
+
+public class MainClass {
+   static String portSelected = "";
+
+   public static void main(String[] args) {
+      configureWindow p = new configureWindow();
+      configureWindow.start();
+   }
+}
